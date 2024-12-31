0dfee361eb27082d9648ad23f6b08ee6c40af945
nhan nguyen <nhanhn1@uci.edu>
2024-12-30 17:15:08 -0800

    added navbar

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+import styles from "./Navbar.module.css";
+
+export const Navbar = () => {
+    return (
+    <nav className={styles.navbar}>
+        <a className={styles.title} href="/">
+            Portfolio
+        </a>
+        <div className={styles.menu}>
+            <ul className={styles.menuItems}>
+                <li>
+                    <a href="#about">About</a>
+                </li>
+                <li>
+                    <a href="#experience">Experience</a>
+                </li>
+                <li>
+                    <a href="#projects">Projects</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+    ); 
+};
